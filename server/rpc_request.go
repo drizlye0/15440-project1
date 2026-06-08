b19cd7c569c2a80cea77d2053250db9b916b78e4
drizlye0 <danielkastillo03@gmail.com>
2026-06-08 18:44:00 -0500

    feat: support for sysFd and data fields

--- server/rpc_request.go
+++ server/rpc_request.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"bytes"
+	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"log"
 )
 
 type RPCRequest struct {
 	callSignature string
+	sysFd         uintptr
 	fileName      string
+	data          []byte
 }
 
 func (r *RPCRequest) encode() bytes.Buffer {
-	format := fmt.Sprintf("%s %s", r.callSignature, r.fileName)
+	hexData := hex.EncodeToString(r.data)
+	format := fmt.Sprintf("%s %d %s %s", r.callSignature, r.sysFd, r.fileName, hexData)
 	buf := bytes.Buffer{}
 	buf.Write([]byte(format))
 	return buf
@@ -22,13 +27,28 @@ func decodeRPCRequest(b *bytes.Buffer) *RPCRequest {
 	cleanedBytes := bytes.TrimSpace(b.Bytes())
 	parts := bytes.Split(cleanedBytes, []byte(" "))
 
-	if len(parts) < 2 {
+	if len(parts) < 4 {
 		log.Println("Failed to parse request: invalid format or missing arguments")
+		return nil
+	}
+
+	ptrArr := parts[1]
+	for i := 0; i < 8-len(ptrArr); i++ {
+		ptrArr = append([]byte{0x0, 0x0}, ptrArr...)
+	}
+	ptr := binary.BigEndian.Uint64(ptrArr)
+
+	hexStr := string(parts[3])
+	decodedData, err := hex.DecodeString(hexStr)
+	if err != nil {
+		log.Printf("Failed to decode hex string data: %v\n", err)
 		return nil
 	}
 
 	return &RPCRequest{
 		callSignature: string(parts[0]),
-		fileName:      string(parts[1]),
+		sysFd:         uintptr(ptr),
+		fileName:      string(parts[2]),
+		data:          decodedData,
 	}
 }
