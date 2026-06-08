25eb47e9594840988fb6efe0b010fb869a7c6a94
drizlye0 <danielkastillo03@gmail.com>
2026-06-08 18:45:26 -0500

    feat: support for sysFd,read,written,error fields

--- server/rpc_response.go
+++ server/rpc_response.go
@@ -6,12 +6,14 @@ import (
 )
 
 type RPCResponse struct {
-	Value any
-	Error error
+	sysFd   uintptr
+	read    []byte
+	written int
+	error   error
 }
 
 func (r *RPCResponse) encode() *bytes.Buffer {
-	format := fmt.Sprintf("%s %s", r.Value, r.Error)
+	format := fmt.Sprintf("%d %v %d %v", r.sysFd, r.read, r.written, r.error)
 	buf := bytes.Buffer{}
 	buf.Write([]byte(format))
 	return &buf
